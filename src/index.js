6d502ab5d4a179686a89bb5d8c4787df62fcee92
Gabriel Christ <gabriel_christ@hotmail.com>
2024-02-05 19:34:01 -0300

    Change from ptbr to eng, remove redundant function(fetchdata) and add situation of number of classes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 const getData = require("./config/getData");
 
-async function fetchData() {
-  try {
-    const data = await getData();
-    return data;
-  } catch (error) {
-    console.error("Erro ao buscar dados:", error);
-  }
-}
-
 async function convertStudentsArrayIntoObject() {
-  const studentsData = await fetchData();
+  const studentsData = await getData();
   const students = studentsData.slice(3);
-  const objetos = students.map(([matricula, nome, faltas, p1, p2, p3]) => {
-    return { matricula, nome, faltas, p1, p2, p3 };
+  const objetos = students.map(([id, name, absences, p1, p2, p3]) => {
+    return {
+      id,
+      name,
+      absences: Number(absences),
+      p1: Number(p1),
+      p2: Number(p2),
+      p3: Number(p3),
+    };
   });
-  manipulateStudentsInformation(objetos);
+  const totalNumberOfClasses = Number(
+    studentsData[1].toString().split(":")[1].trim(),
+  );
+  const minNumberOfClasses = totalNumberOfClasses / 4;
+  console.log(totalNumberOfClasses);
+  manipulateStudentsInformation(objetos, minNumberOfClasses);
 }
 
-function manipulateStudentsInformation(studentsData) {
+function manipulateStudentsInformation(studentsData, minNumberOfClasses) {
   const studentsApprovingStatus = [];
   for (let i = 0; i < studentsData.length; i++) {
     const element = studentsData[i];
-    let gradeAverage =
-      (Number(element.p1) + Number(element.p2) + Number(element.p3)) / 3;
-    if (gradeAverage >= 70) {
-      element.approved = true;
-      element.finalAverage = Number(gradeAverage.toFixed(2));
+    let gradeAverage = (element.p1 + element.p2 + element.p3) / 3;
+    if (minNumberOfClasses < element.faltas) {
+      element.situation = "Reprovado por falta";
+      element.finalAverage = gradeAverage.toFixed(2);
       studentsApprovingStatus.push(element);
     } else {
-      element.approved = false;
-      element.finalAverage = Number(gradeAverage.toFixed(2));
-      studentsApprovingStatus.push(element);
+      if (gradeAverage >= 70) {
+        element.situation = "Aprovado";
+        element.finalAverage = Math.ceil(gradeAverage);
+        studentsApprovingStatus.push(element);
+      } else {
+        element.situation = "Reprovado por nota";
+        element.finalAverage = Math.ceil(gradeAverage);
+        studentsApprovingStatus.push(element);
+      }
     }
   }
   console.log(studentsApprovingStatus);
